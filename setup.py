ecd15dc12259599799d3a69cb97f27caf9b5c36c
oialid25 <oualidamtout2005@gmail.com>
2026-03-12 21:09:40 +0000

    Add new PDF files, update requirements, and enhance trials notebook
    - Added new PDF files: "06-convention g_Omrane.pdf", "4 AMC.pdf", "convention Eqdom.pdf", and "attest-voyage.pdf".
    - Updated requirements.txt with necessary dependencies for the project.
    - Enhanced trials.ipynb with code for loading PDFs, extracting text, and connecting to Pinecone DB.
    - Created setup.py for package distribution.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="oualidAmtout",
+    version="0.1.0",
+    author="oualidAmtout",
+    author_email="oualidamtout2005@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
